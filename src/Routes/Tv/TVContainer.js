4f6ecb6f1f4bbdc1bd37e23a2fa161eddfbf117a
lgh7418 <lgh7418@gmial.com>
2020-08-01 21:55:51 +0900

    TVPresenter and Loader Components

--- src/Routes/Tv/TVContainer.js
+++ src/Routes/Tv/TVContainer.js
@@ -14,13 +14,13 @@ export default class extends React.Component {
   async componentDidMount() {
     try {
       const {
-        data: { result: topRated },
+        data: { results: topRated },
       } = await tvApi.topRated();
       const {
-        data: { result: popular },
+        data: { results: popular },
       } = await tvApi.popular();
       const {
-        data: { result: airingToday },
+        data: { results: airingToday },
       } = await tvApi.airingToday();
       this.setState({
         topRated,
@@ -43,7 +43,7 @@ export default class extends React.Component {
       <TVPresenter
         topRated={topRated}
         popular={popular}
-        aitingToday={airingToday}
+        airingToday={airingToday}
         error={error}
         loading={loading}
       />
